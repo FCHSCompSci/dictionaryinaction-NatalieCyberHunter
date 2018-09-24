6ce517ba3083468e6bce5a9a1327e22050bf04cb
Natalie Christine Hunter <natalie.hunter@stu.jefferson.kyschools.us>
2018-09-24 08:21:33 -0400

    not finished

---
+++ Dictionary.py
@@ -0,0 +1,50 @@
+print ('Mythical Creatures')
+
+phoenix = {
+    'creature':'Phoenix',
+    'color':'orange',
+    'good_or_evil':'good',
+    'eyes':'blue',
+    'power':'regenerates or is born again',
+    'power_val': 9,
+    }
+
+siren = {
+    'creature':'Siren',
+    'color':'green tale',
+    'good_or_evil':'evil',
+    'eyes':'black',
+    'power':'suduce by there voice to kill',
+    'power_value': 10,
+    }
+
+basilisk= {
+    'creature':'Basilisk',
+    'color':'green',
+    'good_or_evil':'evil',
+    'eyes':'yellow',
+    'power':'kills you instantly with one look',
+    'power_value': 8,
+    }
+
+hippo_campus = {
+    'creature':'Hippo Campus',
+    'color':'bluish green',
+    'good_or_evil':'good',
+    'eyes':'grey',
+    'power':'unknown',
+    'power_value': 7,
+    }
+
+power_value = ( 9, 10, 8, 7)
+
+start = input ("press (s) to start")
+
+while True 
+
+
+
+
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            q
+
+
